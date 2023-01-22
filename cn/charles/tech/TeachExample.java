4b37c533715d076c4c749657e7aa356a0909fe1c
charles <charlessevn@gmail.com>
2023-01-22 14:45:42 +0800

    commit tech example

---
+++ cn/charles/tech/TeachExample.java
@@ -0,0 +1,15 @@
+package cn.charles.tech;
+
+public class TeachExample {
+    private static final String s = "lalal";
+
+    private static final String m = "hahaha";
+
+    private static final String l = "asdsa";
+
+    public static void main(String[] args) {
+
+        String dst = s + m + l;
+        System.out.println(dst);
+    }
+}
